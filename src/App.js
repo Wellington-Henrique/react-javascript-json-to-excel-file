651db096466d10019db2e947eb9f2ae975968d91
Wellington Henrique <wellingtonhlc@gmail.com>
2023-03-11 13:31:54 -0300

    Implementação de biblioteca de conversão de JSON e download de arquivo xlsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
+var xlsx = require('xlsx');
+const data = require('./data.json');
+
 function App() {
+  const handleDownload = () => {
+    const fileName = "convertedJsontoExcel";
+
+    var workBook = xlsx.utils.book_new();
+    var workSheet = xlsx.utils.json_to_sheet(data);
+
+    xlsx.utils.book_append_sheet(workBook,workSheet)
+    xlsx.writeFile(workBook, `${fileName}.xlsx`)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>JSON to Excel File</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>AGE</th>
+          </tr>
+        </thead>
+        <tbody>
+            {data.map(info => 
+              <tr key={info.ID}>
+                <td>{info.ID}</td>
+                <td>{info.NAME}</td>
+                <td>{info.AGE}</td>
+              </tr>
+            )}
+        </tbody>
+      </table>
+      <button type='submit' onClick={handleDownload}>Download Excel File</button>
     </div>
   );
 }
